eef32744fd16c566e75076359eb193aa7e916e5e
evan-cloud4453 <evan4453@gmail.com>
2026-05-27 02:20:30 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -565,12 +565,12 @@ function handleFileUpload(event) {
 
 function parseExcelToQuiz(rows) {
     let parts = [
-        { title: "Part 1. 단순 의미 파악", timeLimit: 480, instruction: "제시된 영단어의 정확한 뜻을 고르시오.",example: { q: "inadvertently", options: ["(A) 꼼꼼하게", "(B) 점차로", "(C) 무심코", "(D) 주목할 만하게"], answer: "(C) 무심코" }, questions: [] },
-        { title: "Part 2. 동의어/반의어 파악", timeLimit: 420, instruction: "짝지어진 단어의 관계가 나머지 셋과 다른 하나를 고르시오.",example: { q: "The hotel can easily a----- a large tour group.", options: ["(A) accommodate", "(B) maintenance"], answer: "(A) accommodate" }, questions: [] },
-        { title: "Part 3. 품사/의미 파악", timeLimit: 300, instruction: "제시된 단어의 정확한 품사와 뜻을 고르시오.", example: { q: "consistently", options: ["(A) adj. 지속적인", "(B) n. 일관성", "(C) v. 구성되다", "(D) ad. 지속적으로, 일관되게"], answer: "(D) ad. 지속적으로, 일관되게" },questions: [] },
-        { title: "Part 4. 실전 문맥 파악", timeLimit: 900, instruction: "다음 문장의 빈칸에 들어갈 가장 알맞은 어휘를 고르시오.", example: { q: "Property taxes in Granville, a relatively new, ----- area, are higher than in Powerton.", options: ["(A) considerably", "(B) spaciously", "(C) diligently", "(D) expertly"], answer: "(B) spaciously" },questions: [] },
-        { title: "Part 5. 철자 배열 (Scramble)", timeLimit: 540, instruction: "제시된 한글 뜻과 무작위 알파벳 단서를 보고 올바른 철자를 타이핑 하시오.", example: { q: "할당하다, 배정하다", answer: "allocate" },questions: [] },
-        { title: "Part 6. 철자 입력 주관식", timeLimit: 960, instruction: "해석과 첫 글자 단서를 보고 문맥에 맞는 단어를 입력하시오.", example: { q: "After ten years in the marketing department, Ms. Quinn was finally p----- to director.\n(해석: 마케팅 부서에서 10년을 보낸 후, 퀸 씨는 마침내 이사로 승진되었습니다.)", answer: "promoted" },questions: [] }
+        { title: "Part 1. 단순 의미 파악", timeLimit: 480, instruction: "제시된 영단어의 정확한 뜻을 고르시오.", questions: [] },
+        { title: "Part 2. 동의어/반의어 파악", timeLimit: 420, instruction: "짝지어진 단어의 관계가 나머지 셋과 다른 하나를 고르시오.", questions: [] },
+        { title: "Part 3. 품사/의미 파악", timeLimit: 300, instruction: "제시된 단어의 정확한 품사와 뜻을 고르시오.", questions: [] },
+        { title: "Part 4. 실전 문맥 파악", timeLimit: 900, instruction: "다음 문장의 빈칸에 들어갈 가장 알맞은 어휘를 고르시오.", questions: [] },
+        { title: "Part 5. 철자 배열 (Scramble)", timeLimit: 540, instruction: "제시된 한글 뜻과 무작위 알파벳 단서를 보고 올바른 철자를 타이핑 하시오.", questions: [] },
+        { title: "Part 6. 철자 입력 주관식", timeLimit: 960, instruction: "해석과 첫 글자 단서를 보고 문맥에 맞는 단어를 입력하시오.", questions: [] }
     ];
 
     rows.forEach(row => {
